384ffb19edf1489e73715eebceeb154571938982
waimea-ljmutch <ljmutch@waimea.school.nz>
2025-03-24 10:59:59 +1300

    updated of the assesment

--- src/Main.kt
+++ src/Main.kt
@@ -2,18 +2,69 @@
  * =====================================================================
  * Programming Project for NCEA Level 2, Standard 91896
  * ---------------------------------------------------------------------
- * Project Name:  old gold game
+ * Project Name:  dice game
  * Project Author: Liam Mutch
  * GitHub Repo:    https://github.com/waimea-ljmutch/-level-2-programming-assessment
  * ---------------------------------------------------------------------
  * Notes:
- * This is a two-player game, played on a one-dimensional grid with coins, where the aim is to win by being the player who **removes the gold coin**.
+ * Games that allow players to 'roll' multiple dice to gain points / beat the other player based on the dice rolled...
  * =====================================================================
  */
 
 
 
 fun main() {
-    println("old gold game")
-}
+    println(
+        " ________  ___  ________  _______                \n" +
+                "|\\   ___ \\|\\  \\|\\   ____\\|\\  ___ \\               \n" +
+                "\\ \\  \\_|\\ \\ \\  \\ \\  \\___|\\ \\   __/|              \n" +
+                " \\ \\  \\ \\\\ \\ \\  \\ \\  \\    \\ \\  \\_|/__            \n" +
+                "  \\ \\  \\_\\\\ \\ \\  \\ \\  \\____\\ \\  \\_|\\ \\           \n" +
+                "   \\ \\_______\\ \\__\\ \\_______\\ \\_______\\          \n" +
+                "    \\|_______|\\|__|\\|_______|\\|_______|          \n" +
+                "                                                 \n" +
+                "                                                 \n" +
+                "                                                 \n" +
+                " ________  ________  _____ ______   _______      \n" +
+                "|\\   ____\\|\\   __  \\|\\   _ \\  _   \\|\\  ___ \\     \n" +
+                "\\ \\  \\___|\\ \\  \\|\\  \\ \\  \\\\\\__\\ \\  \\ \\   __/|    \n" +
+                " \\ \\  \\  __\\ \\   __  \\ \\  \\\\|__| \\  \\ \\  \\_|/__  \n" +
+                "  \\ \\  \\|\\  \\ \\  \\ \\  \\ \\  \\    \\ \\  \\ \\  \\_|\\ \\ \n" +
+                "   \\ \\_______\\ \\__\\ \\__\\ \\__\\    \\ \\__\\ \\_______\\\n" +
+                "    \\|_______|\\|__|\\|__|\\|__|     \\|__|\\|_______|"
+    )
+    println("+--------------------------------------------------------------------------------+")
+    println("INSTRUCTIONS")
+    println("so what you have to do is there is two players player 1 & player 2.")
+    println("the first player would go and you would role the die to see what number")
+    println("you get whatever number you get it would add to your points. for the")
+    println("first round there will be no bad numbers so that the round doesnt end")
+    println("quick before both players play first then the round starts you will")
+    println("roll the die and there will be a bad number if you get that number you")
+    println("lose. It will go on until one person looses bye the die or the other person")
+    println("gets the most points ")
+    println("+--------------------------------------------------------------------------------+")
 
+    println("Enter your name player 1: ")
+    val player1 = readln()
+    println("player1: ${player1} ")
+
+
+    println("Enter you name player 2: ")
+    val player2 = readln()
+    println("player2: ${player2} ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
