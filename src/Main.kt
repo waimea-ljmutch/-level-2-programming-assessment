761b23aa176e0d28973e6fe7648adf3d9b11f9cd
waimea-ljmutch <ljmutch@waimea.school.nz>
2025-04-08 15:09:39 +1200

    so close to finishing just need rounds and fixing things

--- src/Main.kt
+++ src/Main.kt
@@ -46,35 +46,13 @@ fun main() {
     println("+--------------------------------------------------------------------------------+")
 
 
-
-
-
     // getting player names
-    println("Enter your name player 1: ")
-    val playername1 = readln()
-    println("player1:  ${playername1} ")
+    val player1 = getString("enter your name player 1:")
+    val player2 = getString("enter your name player 2:")
+    println("welcome to the dice game  $player1 and $player2 ")
 
-
-    println("Enter you name player 2: ")
-    val PlayerName2 = readln()
-    println("player2: ${PlayerName2} ")
-
-    println("welcome to the dice game  ${playername1} and ${PlayerName2} ")
-
-    //rolling the die
-fun rollDice(playerName: String ): {
-    println("please press x to roll the dice $playerName")
-    println()
-    val
-
-    }
-
-
-
-    1..6).random()
-
-   // getting a list
-    val dice= mutableListOf<String>()
+// getting a list
+    val dice = mutableListOf<String>()
 
     dice.add(1.toString())
     dice.add(2.toString())
@@ -82,15 +60,46 @@ fun rollDice(playerName: String ): {
     dice.add(4.toString())
     dice.add(5.toString())
     dice.add(6.toString())
+    dice.add(7.toString())
+    dice.add(8.toString())
+    dice.add(9.toString())
+    dice.add(10.toString())
     println(dice)
 
 
+    var score1 = 0
+    var score2 = 0
+
+
+    for (round in 1..8) {
+        println("\n+--- Round $round ---+")
+
+        // Player 1's Turn
+        score1 += rollDice(player1)
+
+        // Player 2's Turn
+        score2 += rollDice(player2)
+
+        var player1Points = 0
+        var player2Points = 0
+
+        //main game loop
+        var currentPlayer = 1
+        var roundpoints = 0
+
+        fun rollDice(userName: String): Int {
+            print("$userName, Please type 'r' to roll ")
+            readln() // Wait for the users input
+            val roll = rollDice().random().toInt() // Get a random number between 1 - 10
+            println("$userName rolled a $roll")
+            return roll
+        }
 
 
     fun getString(prompt: String): String {
         var userInput: String
 
-        while(true) {
+        while (true) {
             print(prompt)
 
             userInput = readln()
@@ -99,12 +108,13 @@ fun rollDice(playerName: String ): {
 
         return userInput
     }
-
-
 }
-//rolling the die
-fun rollDice(playerName: String ): {
-    println("please press x to roll the dice")
-    println()
 
-}
+    // Function to simulate a dice roll
+    fun rollDice(): Int {
+        
+    }
+
+
+
+
